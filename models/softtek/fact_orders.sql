faf797288f5fd5f7908944f8250fcc90c5b92e07
Mariox09mx <mvargascarballo@gmail.com>
2023-03-30 09:44:38 -0700

    a

--- models/softtek/fact_orders.sql
+++ models/softtek/fact_orders.sql
@@ -4,7 +4,7 @@ select a.O_ORDERKEY,
 a.O_CUSTKEY,
 b.N_NAME,
 a.O_TOTALPRICE,
-a.O_ORDERDATE,
+a.O_ORDERDATE
 from {{ref('orders')}} a
 left join {{ref('dim_customers')}} b
 where a.O_CUSTKEY = b.C_CUSTKEY
