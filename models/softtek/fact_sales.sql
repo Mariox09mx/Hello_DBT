8abdafb72b76d3cbb5ebb72b703d93f6aeceeb29
Mariox09mx <mvargascarballo@gmail.com>
2023-04-13 20:44:06 -0700

    a

--- models/softtek/fact_sales.sql
+++ models/softtek/fact_sales.sql
@@ -16,3 +16,5 @@ SELECT
   CURRENT_TIMESTAMP() as audit_update_date
 FROM {{source('src_sales')}}
 {% if is_incremental() %}
+WHERE update_src_date > (SELECT update_src_date FROM {{ref('fact_sales')}})
+{% endif %}
